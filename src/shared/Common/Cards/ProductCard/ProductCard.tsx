3e34730a3ad0c2a02980c219cf186229f944778a
Saied <elsaied.samaka1997@gmail.com>
2023-10-12 14:31:33 +0300

    add the product card component

--- src/shared/Common/Cards/ProductCard/ProductCard.tsx
+++ src/shared/Common/Cards/ProductCard/ProductCard.tsx
@@ -4,26 +4,26 @@ import Link from "next/link";
 const ProductCard = ({ id }) => {
   return (
     <li>
-      <Link href={`/cars/${encodeURIComponent(id)}`}>
-        <div className="relative bg-white max-w-lg p-5 flex  flex-col rounded-2xl h-44 md:h-80">
-          {/* product title */}
-          <div className="product-title flex items-center justify-between">
-            <h4 className="text-xl font-bold">Product</h4>
-            <svg
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M19.8164 5.1838L19.8164 5.18386C21.7279 7.095 21.7279 10.207 19.816 12.1182L19.8159 12.1183L11.9997 19.9345L4.18359 12.1183C2.27226 10.2066 2.27205 7.09447 4.18348 5.18342C6.09512 3.27216 9.20731 3.27224 11.1184 5.18337C11.2247 5.28962 11.3257 5.4004 11.422 5.51699L11.9992 6.21548L12.5775 5.5179C12.6749 5.40047 12.7762 5.28905 12.8814 5.18386C14.793 3.2726 17.9052 3.27267 19.8164 5.1838Z"
-                stroke="#A4A5A6"
-                strokeWidth="1.5"
-              />
-            </svg>
-          </div>
-          {/* product title */}
+      <div className="relative bg-white max-w-lg p-5 flex  flex-col rounded-2xl h-44 md:h-80">
+        {/* product title */}
+        <div className="product-title flex items-center justify-between">
+          <h4 className="text-xl font-bold">Product</h4>
+          <svg
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19.8164 5.1838L19.8164 5.18386C21.7279 7.095 21.7279 10.207 19.816 12.1182L19.8159 12.1183L11.9997 19.9345L4.18359 12.1183C2.27226 10.2066 2.27205 7.09447 4.18348 5.18342C6.09512 3.27216 9.20731 3.27224 11.1184 5.18337C11.2247 5.28962 11.3257 5.4004 11.422 5.51699L11.9992 6.21548L12.5775 5.5179C12.6749 5.40047 12.7762 5.28905 12.8814 5.18386C14.793 3.2726 17.9052 3.27267 19.8164 5.1838Z"
+              stroke="#A4A5A6"
+              strokeWidth="1.5"
+            />
+          </svg>
+        </div>
+        {/* product title */}
+        <Link href={`/cars/${encodeURIComponent(id)}`}>
           {/* product description */}
           <span className="text-gray-500">product description</span>
           {/* product description */}
@@ -81,8 +81,8 @@ const ProductCard = ({ id }) => {
             {/* right content */}
           </div>
           {/* product details */}
-        </div>
-      </Link>
+        </Link>
+      </div>
     </li>
   );
 };
